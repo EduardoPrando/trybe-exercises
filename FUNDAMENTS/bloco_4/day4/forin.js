3435c06c8564f2537d4676571b4fded29a382815
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2021-05-24 17:58:31 -0300

    Add folder day4 and exercies

---
+++ FUNDAMENTS/bloco_4/day4/forin.js
@@ -0,0 +1,35 @@
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let i in cars) {
+//   console.log(cars[i]);
+// }
+
+// let car = {
+//   type: 'Fiat',
+//   model: '500',
+//   color: 'White',
+// };
+
+// for (let i in car) {
+//   console.log(i, car[i]);
+// }
+
+// Exercise 1
+// let names = { 
+//   person1: 'João',
+//   person2: 'Maria',
+//   person3: 'Jorge'
+// }
+// for (let i in names){
+//   console.log('Olá ', names[i]);
+// }
+
+// Exercise 2
+// let car = {
+//   model: 'A3 Sedan',
+//   manufacturer: 'Audi',
+//   year: 2020
+// }
+// for (let i in car){
+//   console.log(i, car[i])
+// }
