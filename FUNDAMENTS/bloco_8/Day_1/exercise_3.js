68ed50b76e8bd408d144ae9da426eaa551444de7
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2021-06-27 18:36:04 -0300

    bloco 8 day 1 and 2

---
+++ FUNDAMENTS/bloco_8/Day_1/exercise_3.js
@@ -0,0 +1,19 @@
+/*  3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+
+//   Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.*/
+
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+function counting (rightAnswers, studentAnswers) {
+  let counting = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    if (rightAnswers[index] === studentAnswers[index]) counting += 1;
+     if (rightAnswers[index] !== studentAnswers[index]) counting -= 0.5;
+     if (studentAnswers[index] === 'N.A') counting += 0;
+  } return counting;
+};
+
+const studantScore = (counting(rightAnswers, studentAnswers));
+
+console.log(studantScore);
--- FUNDAMENTS/bloco_8/Day_2/day_2.js
+++ FUNDAMENTS/bloco_8/Day_2/day_2.js
@@ -60,3 +60,44 @@
 // console.log(hasName(names, 'José'))
 
 /*  2 - Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false , use every ;*/
+
+// const people = [
+//   { name: 'Mateus', age: 18 },
+//   { name: 'José', age: 16 },
+//   { name: 'Ana', age: 23 },
+//   { name: 'Cláudia', age: 20 },
+//   { name: 'Bruna', age: 19 },
+// ];
+
+// const verifyAges = (arr, minimumAge) => arr.every((arr)=> arr.age >= minimumAge)
+
+// console.log(verifyAges(people, 18));
+
+// ------------Array.sort
+/*  1 - Utilize a sort para ordenar o array pela idade das pessoas em ordem crescente .*/
+
+// const people = [
+//   { name: 'Mateus', age: 18 },
+//   { name: 'José', age: 16 },
+//   { name: 'Ana', age: 23 },
+//   { name: 'Cláudia', age: 20 },
+//   { name: 'Bruna', age: 19 },
+// ];
+
+// people.sort((a, b) => a.age - b.age);
+
+// console.log(people);
+
+/*  2 - Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente . */
+
+// const people = [
+//   { name: 'Mateus', age: 18 },
+//   { name: 'José', age: 16 },
+//   { name: 'Ana', age: 23 },
+//   { name: 'Cláudia', age: 20 },
+//   { name: 'Bruna', age: 19 },
+// ];
+
+// people.sort((a, b) => b.age - a.age);
+
+// console.log(people);
